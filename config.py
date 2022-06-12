e412c3e58dad933bdd412feb415baac4d8ede4c7
Wisnu Wardana <61041182+wisnuwrdna@users.noreply.github.com>
2022-06-12 14:48:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from os import environ
 
 ### Getting ENvironment Variables
 class Config(object):
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") # Make a bot from https://t.me/BotFather
+    BOT_TOKEN = environ.get("BOT_TOKEN", "5456385503:AAHl0cpUACo-rhLwMLgQTDiOK9pJdRYA8JA") # Make a bot from https://t.me/BotFather
     
-    APP_ID = int(environ.get("API_ID", "0")) # Get this value from https://my.telegram.org/apps
+    APP_ID = int(environ.get("API_ID", "2515895")) # Get this value from https://my.telegram.org/apps
     
-    API_HASH = environ.get("API_HASH", "") # Get this value from https://my.telegram.org/apps
+    API_HASH = environ.get("API_HASH", "503118adea978a8d0f82b598558382102515895") # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(environ.get("OWNER_ID", None)) # Your(owner's) telegram id
+    OWNER_ID = int(environ.get("OWNER_ID", 1849008944)) # Your(owner's) telegram id
     
     MONGO_STR = environ.get("MONGO_STR", "") # Get from MongoDB Atlas
 
